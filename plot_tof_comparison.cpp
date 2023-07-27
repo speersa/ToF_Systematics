66e27935aa66d48a2d7c0301d6516a1f401199fe
speersa <a.speers@lancaster.ac.uk>
2023-07-27 10:22:39 +0100

    new comparison code, but slightly broken

---
+++ plot_tof_comparison.cpp
@@ -0,0 +1,50 @@
+
+
+void plot_tof_comparison(){
+	
+	gROOT->SetBatch();
+
+	TFile *p7 = new TFile("ToF_Output_P7_run4.root");
+	TIter nextp7(p7->GetListOfKeys());
+	TKey *keyp7 = NULL;
+	
+	TFile *p6 = new TFile("ToF_Output_P6_run4.root");
+	TIter nextp6(p6->GetListOfKeys());
+	TKey *keyp6 = NULL;
+	
+	TCanvas *canv = new TCanvas("canv", "canv", 1024, 1024);
+	TString pdfname = "ToF_Plots_comparison.pdf"; // name of pdf
+	canv->Print(pdfname+"["); // open multi-page pdf
+	
+	
+	while ((keyp7 = (TKey*)nextp7())) {
+		keyp6 = (TKey*)nextp6();
+		std::string classname = keyp7->GetClassName();
+		// Check the class name to be sure
+		if (classname.find("TH1D") == std::string::npos) continue;
+
+		auto legend = new TLegend(0.1,0.7,0.48,0.9);		
+		THStack *hs = new THStack("hs",Form("%s",keyp7->GetName()));
+		TH1D *temp1 = (TH1D*)gDirectory->Get(keyp7->GetName());
+		temp1->SetLineColor(kBlue);
+		legend->AddEntry(temp1, "Production 7", "l");
+		hs->Add(temp1);
+		
+		TH1D *temp2 = (TH1D*)gDirectory->Get(keyp6->GetName());
+		temp2->SetLineColor(kRed);
+		legend->AddEntry(temp2, "Production 6", "l");
+		hs->Add(temp2);
+		// Or use key->ReadObj()...
+		
+		canv->cd();
+		hs->Draw();		
+		legend->Draw();
+		
+		canv->Print(pdfname); // add new page to pdf
+		canv->SaveAs(Form("ToF_plots/%s.png",keyp7->GetName()));
+	}
+	
+	canv->Print(pdfname+"]"); // close multi-page pdf
+
+
+}
